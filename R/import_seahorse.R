6bddb2a37f7a08d0e4ab0cd43b72c0120843f018
Xiang Zhang <sc.xiang.zhang@gmail.com>
2020-04-18 21:57:27 +0200

    Initialize R package

---
+++ R/import_seahorse.R
@@ -0,0 +1,26 @@
+#' Import Seahorse data
+#' 
+#' Load Seahorse Excel files into R as a data frame
+#' 
+#' Import the Excel spreadsheet which contains the Seahorse XF assay result
+#' @param x Excel spreadsheet which contains the Seahorse XF assay result
+#' @import readxl
+#' @export
+#' @example 
+#' import_seahorse("seahorse_c2c12.xlsx")
+#' 
+#' @return This function returns a \code{data.frame} including columns:
+#' \itemize{
+#'   \item Measurement 
+#'   \item Well 
+#'   \item Group 
+#'   \item Time 
+#'   \item OCR 
+#'   \item ECAR 
+#'   \item PER 
+#' }
+#' 
+
+import_seahorse <- function(x) {
+  read_excel(x, sheet = "Rate")
+}
---
+++ R/rwave.R
@@ -0,0 +1,7 @@
+#' rwave: Seahorse Wave in R
+#'
+#' A R package for Seahorse data visualization and statistical analysis
+#'
+#' @docType package
+#' @name rwave
+"_PACKAGE"
