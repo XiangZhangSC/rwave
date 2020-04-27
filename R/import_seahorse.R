daca9df4eb75eac84c602bd4d163223507c59895
Xiang Zhang <sc.xiang.zhang@gmail.com>
2020-04-27 21:34:52 +0200

    change the import_seahorse function so that both Rate and Raw data will be loaded

--- R/import_seahorse.R
+++ R/import_seahorse.R
@@ -4,8 +4,16 @@
 #' 
 #' Import the Excel spreadsheet which contains the Seahorse XF assay result
 #' @param x Excel spreadsheet which contains the Seahorse XF assay result
+#' @return a list with two \code{data.frame} corresponding to the Rate and Raw data
 #' @import readxl
 #' @export
 import_seahorse <- function(x) {
-  read_excel(x, sheet = "Rate")
+  dat_rate <- read_excel(x, sheet = "Rate")
+  dat_raw <- read_excel(x, sheet = "Raw")
+  
+  dat_seahorse <- list()
+  dat_seahorse$Rate <- dat_rate
+  dat_seahorse$Raw <- dat_raw
+  
+  return(dat_seahorse)
 }
