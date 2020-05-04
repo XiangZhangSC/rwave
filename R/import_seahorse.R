24940810d25d3ad57a4c7f9f72e0174aeedfed47
Xiang Zhang <sc.xiang.zhang@gmail.com>
2020-05-04 16:37:05 +0200

    fix the start and end time of operation log data

--- R/import_seahorse.R
+++ R/import_seahorse.R
@@ -6,6 +6,8 @@
 #' @param x Excel spreadsheet which contains the Seahorse XF assay result
 #' @return a list with two \code{data.frame} corresponding to the Rate and Raw data
 #' @import readxl
+#' @import dplyr
+#' @import lubridate
 #' @export
 import_seahorse <- function(x) {
   dat_rate <- read_excel(x, sheet = "Rate")
@@ -15,7 +17,13 @@ import_seahorse <- function(x) {
   dat_seahorse <- list()
   dat_seahorse$Rate <- dat_rate
   dat_seahorse$Raw <- dat_raw
-  dat_seahorse$Log <- dat_log
+  
+  updated_dat_log <- dat_log %>% 
+    mutate(`Start Time` = dmy_hms(`Start Time`), 
+           `End Time` = dmy_hms(`End Time`), 
+           `Command Index` = as.integer(`Command Index`))
+  
+  dat_seahorse$Log <- updated_dat_log
   
   return(dat_seahorse)
 }
