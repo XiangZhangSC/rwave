98fd693aef4454aae590ec8984282800c015831a
Xiang Zhang <sc.xiang.zhang@gmail.com>
2020-05-20 11:19:17 +0200

    remove the log data import

--- R/import_seahorse.R
+++ R/import_seahorse.R
@@ -7,23 +7,14 @@
 #' @return a list with two \code{data.frame} corresponding to the Rate and Raw data
 #' @import readxl
 #' @import dplyr
-#' @import lubridate
 #' @export
 import_seahorse <- function(x) {
   dat_rate <- read_excel(x, sheet = "Rate")
   dat_raw <- read_excel(x, sheet = "Raw")
-  dat_log <- read_excel(x, sheet = "Operation Log")
   
   dat_seahorse <- list()
   dat_seahorse$Rate <- dat_rate
   dat_seahorse$Raw <- dat_raw
-  
-  updated_dat_log <- dat_log %>% 
-    mutate(`Start Time` = dmy_hms(`Start Time`), 
-           `End Time` = dmy_hms(`End Time`), 
-           `Command Index` = as.integer(`Command Index`))
-  
-  dat_seahorse$Log <- updated_dat_log
   
   return(dat_seahorse)
 }
