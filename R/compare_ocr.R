d840fa043c978385bf112c6a016c2ab6f7dfc068
Xiang Zhang <sc.xiang.zhang@gmail.com>
2020-05-28 15:46:30 +0200

    added parametric tests

--- R/compare_ocr.R
+++ R/compare_ocr.R
@@ -6,6 +6,8 @@
 #' When there are three groups, the Kruskal–Wallis one-way analysis of variance will be sued for the group comparison.
 #' 
 #' @param df a \code{data.frame} with raw data and produced by \code{import_seahorse}
+#' @param robust a logical variable which indicates non-parametric tests will be used if it is TRUE by default.
+#' when robust = FALSE, t test or anova will be used depending on the number of groups
 #' @param stats.only a \code{logical} value that indicates whether only the statistical outcomes are returned
 #' 
 #' @import dplyr
@@ -15,7 +17,7 @@
 #' 
 #' @export
 
-compare_ocr <- function(df, stats.only = FALSE) {
+compare_ocr <- function(df, robust = TRUE, stats.only = FALSE) {
   
   # calculate APR
   ocr_df <- summarize_ocr(df)
@@ -28,13 +30,25 @@ compare_ocr <- function(df, stats.only = FALSE) {
     group_by(what) %>% 
     nest()
   
-  if (n_groups >= 3) {
-    ocr_res <- ocr_nest %>% 
-      mutate(stat = map(data, ~tidy(kruskal.test(val~Group, data = .))))
-  } else {
-    ocr_res <- ocr_nest %>% 
-      mutate(stat = map(data, ~tidy(wilcox.test(val~Group, data = .))))
+  if (robust == TRUE) {
+    if (n_groups >= 3) {
+      ocr_res <- ocr_nest %>% 
+        mutate(stat = map(data, ~tidy(kruskal.test(val~Group, data = .))))
+    } else {
+      ocr_res <- ocr_nest %>% 
+        mutate(stat = map(data, ~tidy(wilcox.test(val~Group, data = .))))
+    }
+  } else if (robust == FALSE) {
+    if (n_groups >= 3) {
+      ocr_res <- ocr_nest %>% 
+        mutate(stat = map(data, ~tidy(aov(val~Group, data = .)))) %>% 
+        filter(term == "Group")
+    } else {
+      ocr_res <- ocr_nest %>% 
+        mutate(stat = map(data, ~tidy(t.test(val~Group, data = ., paired = FALSE))))
+    }
   }
+  
   
   # output
   
