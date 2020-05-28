961e40357490bae0681e3f7be7af93da22ef45bc
Xiang Zhang <sc.xiang.zhang@gmail.com>
2020-05-28 15:48:13 +0200

    the function can sketch at either data level or experimental group level

--- R/sketch_ecar.R
+++ R/sketch_ecar.R
@@ -4,19 +4,38 @@
 #' 
 #' @import ggplot2
 #' @param df a \code{data.frame} produced by \code{import_seahorse}
+#' @param which_level a character either "data" or "group"
 #' @param trend a logical value indicates whether the median is used to represent the trend
 #' @return This function returns line plots of ECAR
 #' @export
-sketch_ecar <- function(df, trend = TRUE) {
-  g0 <- ggplot(df, aes(Time, ECAR)) + 
-    facet_wrap(~Group) + 
-    labs(x = "Time (min)", y = "ECAR (mpH/min)") + 
-    theme_classic()
-  
-  if (trend == TRUE) {
-    g0 + geom_line(aes(group = Well), alpha = 0.2) + 
-      stat_summary(fun = median, geom = "line", aes(group = Group))
-  } else {
-    g0 + geom_line(aes(group = Well))
+sketch_ecar <- function(df, which_level = "group", trend = TRUE) {
+  if (which_level == "data") {
+    g0 <- ggplot(df, aes(Time, ECAR)) + 
+      facet_wrap(~Group) + 
+      labs(x = "Time (min)", y = "ECAR (mpH/min)") + 
+      theme_classic()
+    
+    if (trend == TRUE) {
+      g0 + geom_line(aes(group = Well), alpha = 0.2) + 
+        stat_summary(fun = median, geom = "line", aes(group = Group))
+    } else {
+      g0 + geom_line(aes(group = Well))
+    }
+  } else if (which_level == "group") {
+    df_group <- df %>% 
+      select(Well, Group, Time, ECAR) %>% 
+      group_by(Group, Time) %>% 
+      summarize(mean_ecar = mean(ECAR), 
+                sd_ecar = sd(ECAR)) %>% 
+      ungroup()
+    
+    ggplot(df_group, aes(Time, mean_ecar, color = Group)) + 
+      geom_line(aes(group = Group)) + 
+      geom_pointrange(aes(ymin = mean_ecar - sd_ecar, ymax = mean_ecar + sd_ecar)) +
+      scale_color_brewer(palette = "Set1") +
+      labs(x = "Time (min)", y = "ECAR (mpH/min)") + 
+      theme_classic() + 
+      theme(legend.position = "top")
   }
+  
 }
--- R/sketch_ocr.R
+++ R/sketch_ocr.R
@@ -4,19 +4,38 @@
 #' 
 #' @import ggplot2
 #' @param df a \code{data.frame} produced by \code{import_seahorse}
-#' @param trend a logical value indicates whether the median is used to represent the trend
+#' @param which_level a character either "data" or "group"
+#' @param trend a logical value indicates whether the median of replicates will be used
 #' @return This function returns line plots of OCR
 #' @export
-sketch_ocr <- function(df, trend = TRUE) {
-  g0 <- ggplot(df, aes(Time, OCR)) + 
-    facet_wrap(~Group) + 
-    labs(x = "Time (min)", y = "OCR (pmol/min)") + 
-    theme_classic()
-  
-  if (trend == TRUE) {
-    g0 + geom_line(aes(group = Well), alpha = 0.2) + 
-      stat_summary(fun = median, geom = "line", aes(group = Group))
-  } else {
-    g0 + geom_line(aes(group = Well))
+sketch_ocr <- function(df, which_level = "group", trend = TRUE) {
+  if (which_level == "data") {
+    g0 <- ggplot(df, aes(Time, OCR)) + 
+      facet_wrap(~Group) + 
+      labs(x = "Time (min)", y = "OCR (pmol/min)") + 
+      theme_classic()
+    
+    if (trend == TRUE) {
+      g0 + geom_line(aes(group = Well), alpha = 0.2) + 
+        stat_summary(fun = median, geom = "line", aes(group = Group))
+    } else {
+      g0 + geom_line(aes(group = Well))
+    }
+  } else if (which_level == "group") {
+    df_group <- df %>% 
+      select(Well, Group, Time, OCR) %>% 
+      group_by(Group, Time) %>% 
+      summarize(mean_ocr = mean(OCR), 
+                sd_ocr = sd(OCR)) %>% 
+      ungroup()
+    
+    ggplot(df_group, aes(Time, mean_ocr, color = Group)) + 
+      geom_line(aes(group = Group)) + 
+      geom_pointrange(aes(ymin = mean_ocr - sd_ocr, ymax = mean_ocr + sd_ocr)) + 
+      scale_color_brewer(palette = "Set1") +
+      labs(x = "Time (min)", y = "OCR (pmol/min)") + 
+      theme_classic() + 
+      theme(legend.position = "top")
   }
+  
 }
