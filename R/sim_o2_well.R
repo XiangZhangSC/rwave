2b4782065b83f26003d501cf88a20e299b5ad0fe
Xiang Zhang <sc.xiang.zhang@gmail.com>
2020-06-19 12:53:20 +0200

    modified the ODE model to simulate the mixing & wait periods between measurement periods

--- R/sim_o2_well.R
+++ R/sim_o2_well.R
@@ -2,19 +2,26 @@
 #'
 #' sim_o2_well simulates the O2 emission fluoresence of a single well
 #'
-#' @param ocr a vector of true biological oxygen consumption rates
+#' @param ocr a vector of true biological oxygen consumption rates, mmHg/s
 #' @param tick.num the number of ticks within each measurement period
 #' @param tick.interval the time interval between two ticks in a measurement period
+#' @param mixing_wait_period the time interval between two measurement periods
+#' @param additional_diffusion the extra diffusion rate when the probe position is up
 #' @import deSolve
 #' @import dplyr
 #' @import tibble
+#' @import purrr
 
-sim_o2_well <- function(ocr, tick.num = 15, tick.interval = 14) {
-    TauP <- 43    # probe response time constant
-    TauC <- 246   # From the wall to the medium
-    TauAC <- 746  # From the atmosphere to the medium
-    TauW <- 296   # From the medium to the wall
-    o2_0 <- 214   # uM, Ambient o2 concentration after calibration
+sim_o2_well <- function(ocr, 
+                        tick.num = 15, 
+                        tick.interval = 14, 
+                        mixing_wait_period = 120, 
+                        additional_diffusion = 0.05) {
+    TauP <- 43          # probe response time constant
+    TauC <- 246         # From the wall to the medium
+    TauAC <- 746        # From the atmosphere to the medium
+    TauW <- 296         # From the medium to the wall
+    o2_0 <- 151.6900241 # mmHg, Ambient o2 concentration after calibration
     
     
     num.injections <- length(ocr) - 1           # number of injections (not including background)
@@ -28,8 +35,28 @@ sim_o2_well <- function(ocr, tick.num = 15, tick.interval = 14) {
     
     # define the o2 data points to be simulated
     dat.sim.init <- data.frame(Measurement = rep(measurement_periods, each = tick.num), 
-                               Tick = tick, 
-                               time = tick.interval * tick)
+                               Tick = tick)
+    # between the last point of a measurement phase 
+    # and the first point of the next measurement phase 
+    # there is a "mixing & wait period"
+    dat.sim.init <- dat.sim.init %>% 
+        mutate(time = tick.interval * tick + mixing_wait_period * (Measurement - 1))
+                               
+    mixing_wait_start <- dat.sim.init %>% 
+        filter(Measurement < num.measurement) %>% 
+        group_by(Measurement) %>% 
+        summarize(time = max(time))
+    
+    mixing_wait_end <- dat.sim.init %>% 
+        filter(Measurement > 1) %>% 
+        group_by(Measurement) %>% 
+        summarize(time = min(time))
+    
+    mixing_wait_period <- tibble(mixing_start = mixing_wait_start$time, 
+                                 mixing_end = mixing_wait_end$time) %>% 
+        mutate(time = map2(mixing_start, mixing_end, ~ seq(from = .x, to = .y, by = 1))) %>% 
+        select(time) %>% 
+        unnest(time)
     
     # OCR(t)
     ## step 1 create a time-series
@@ -40,17 +67,33 @@ sim_o2_well <- function(ocr, tick.num = 15, tick.interval = 14) {
     ## step 2 create the interpolating function
     OCR <- approxfun(true_ocr_df, rule = 2)
     
+    # probe_position(t)
+    ## When the probe is at the low position, measuring is on
+    probe_position_df <- data.frame(
+        time = seq(from = 0, to = max(dat.sim.init$time), by = 1), 
+        measuring_position = 1L
+    )
+    ## When the probe is at the high position, measuring is off
+    ## During this period, we assume that the diffusion rate from atomosphere to 
+    ## chamber will be larger
+    probe_position_df$measuring_position[probe_position_df$time %in% mixing_wait_period$time] <- 0L
+    probe_position <- approxfun(probe_position_df, rule = 2)
+    
+    # initial states
     state <- c(O2M = o2_0, 
                O2C = o2_0, 
                O2W = o2_0)
     
-    times <- dat.sim.init$time
+    # time points that we want a solution from the ODE model
+    times <- seq(from = 0, to = max(dat.sim.init$time), by = 1)
     
+    # ODE model
     gerencser <- function(t, state, parameters) {
         with(as.list(c(state, parameters)), {
             ocr <- OCR(t)
+            measuring <- probe_position(t)
             dO2M = k_p * (O2C - O2M)
-            dO2C = k_c * (O2W - O2C) + k_ac * (o2_0 - O2C) - ocr
+            dO2C = k_c * (O2W - O2C) + (k_ac + (1 - measuring) * additional_diffusion) * (o2_0 - O2C) - ocr
             dO2W = k_w * (O2C - O2W) + k_aw * (o2_0 - O2W)
             
             list(c(dO2M, dO2C, dO2W), true_OCR = ocr)
@@ -63,35 +106,21 @@ sim_o2_well <- function(ocr, tick.num = 15, tick.interval = 14) {
                     k_w = 1 / TauW, 
                     k_aw = 0)
     
-    # Events
-    event_times <- dat.sim.init %>% 
-        filter(Measurement > 1) %>% 
-        group_by(Measurement) %>% 
-        summarize(time = min(time))
-    
-    ## Starting from the 2nd measurement
-    ## O2C at the first tick will be forced to be ambient O2
-    mixing <- data.frame(
-        var = "O2C", 
-        time = event_times$time, 
-        value = o2_0, 
-        method = "replace"
-    )
-    
-    out <- ode(y = state, times = times, func = gerencser, parms = parameters, events = list(data = mixing)) 
+    # Solve the ODE
+    out <- ode(y = state, times = times, func = gerencser, parms = parameters) 
     dat.sim <- as.data.frame(out)
     
+    # Convert to O2 eimission
     # parameters to link o2 concentration to fluoresence
-    O2_0_mmHg <-151.6900241
     F0 <- 54025.1441058472
     targetEmission <- 12500
-    Ksv <- (1/O2_0_mmHg)*(F0/targetEmission -1)
+    Ksv <- (1/o2_0)*(F0/targetEmission -1)
     
     # simulate the true fluoresence of a single well
     dat.sim %>% 
         mutate(true_fluoresence = map_dbl(O2C, ~ F0 / (.x * Ksv + 1))) %>% 
         tbl_df() %>% 
-        left_join(dat.sim.init, by = "time") %>% 
+        right_join(dat.sim.init, by = "time") %>% 
         select(Measurement, Tick, everything())
 }
 
