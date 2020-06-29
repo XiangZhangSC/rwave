a1c7112880fb52ce024835d426026da20c5e5cdb
Xiang Zhang <sc.xiang.zhang@gmail.com>
2020-06-29 22:21:56 +0200

    Start a function assigning a group label for every well

---
+++ R/tag_well.R
@@ -0,0 +1,16 @@
+#' Assign experiment group to a well
+#'
+#' @param experiment_setup a data.frame with two columns, group label and number of replicates
+#'
+#' @import dplyr
+#' @import purrr
+#'
+tag_well <- function(experiment_setup) {
+    experiment_setup %>% 
+        filter(Group != "Background") %>% 
+        mutate(Tag = map2(Group, n, ~rep(.x, each = .y))) %>% 
+        select(Tag) %>% 
+        unnest(Tag) %>% 
+        unlist() %>% 
+        as.character()
+}
