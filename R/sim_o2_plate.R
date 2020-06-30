21ae50ed301d654182f99e4f3fa29a0b41507640
Xiang Zhang <sc.xiang.zhang@gmail.com>
2020-06-30 16:10:05 +0200

    Simulate true O2 emission, then adding fluoresence deviation due to well and injection

--- R/sim_o2_plate.R
+++ R/sim_o2_plate.R
@@ -1,15 +1,11 @@
 #' Simulate the O2 emission of a Seahorse assay plate
 #'
-#' sim_o2_plate simulated the O2 emission in a 96-well plate. 
-#' The baseline true biological oxygen consumption rate is drawn from a uniform distribution. 
+#' sim_o2_plate simulated the O2 emission in a 96-well plate with systematic 
+#' errors due to well and injection
 #' 
 #' @param experiment_setup a data.frame with two columns, group label and number of replicates
-#' @param between_well_variation the standard deviation of fluoresence between different wells
-#' @param within_well_variation the standard deviation of fluoresence within a well
+#' @param baseline_ocr a vector of true biological OCR values without any injection
 #' @param num.injections the number of injections will be used in the seahorse assay
-#' @param min.ocr the minimum true biological oxygen consumption rate
-#' @param max.ocr the maximum true biological oxygen consumption rate
-#' @param background.ocr the true biological oxygen consumption rate in the background wells
 #' @import tibble
 #' @import dplyr
 #' @import tidyr
@@ -17,72 +13,44 @@
 #' @import stringr
 #' @export
 sim_o2_plate <- function(experiment_setup, 
-                         between_well_variation, 
-                         within_well_variation, 
-                         num.injections = 4, 
-                         min.ocr = 0.1, 
-                         max.ocr = 0.3, 
-                         background.ocr = 0.1) {
+                         baseline_ocr, 
+                         num.injections = 4) {
     
-    # plate set up
-    well.row <- LETTERS[1:8]
+    # pipette cells from different experimental group into a 96-well plate
+    plate_layout <- seed_plate(experiment_setup)
     
-    well.col <- as.character(seq(from = 1, to = 12, by = 1))
-    well.col <- str_pad(well.col, width = 2, side = "left", pad = "0")
+    # cells cultured with different experimental groups may have different 
+    # true biological OCR corresponding to different ture o2 emission
+    plate.sim <- plate_layout %>% 
+        add_true_ocr(baseline_ocr) %>% 
+        mutate(simulated_data = map(data, ~sim_o2_well(.x$true_ocr)))
     
-    plate <- expand.grid(well.row, well.col)
-    colnames(plate) <- c("row_id", "column_id")
-    well.id <- paste0(plate$row_id, plate$column_id)
+    ## There is a long period before the 1st measurement.
+    ## As a consequence, 10,000 fluoresence units can mean different levels of O2 
+    ## in different wells. 
+    plate.sim$deviation_fluoresence_well <- rnorm(96, mean = 0, sd = 1000)
     
-    # The four corner wells are "background"
-    corner_wells <- well.id %in% c("A01", "A12", "H01", "H12")
-    non_corner_wells <- !corner_wells
-    
-    # every well represents a sample from a specific experimental group
-    # when an injection was added the biological OCR is boosted or suppressed
-    # the effect is the same for different groups
-    effect_size_injections = runif(num.injections, min = 0, max = 2)
-    
-    # Depsite calibration, there is a long period before the 1st measurement.
-    # As a consequence, 10,000 fluoresence units can mean different levels of O2 
-    # in different wells. 
-    num.well <- length(well.id)
-    bias_well <- rnorm(num.well, mean = 0, sd = between_well_variation)
-    
-    well_property <- experiment_setup %>% 
-        mutate(baseline_ocr = runif(num.injections, min = min.ocr, max = max.ocr), 
-               group_label = map2(Group, n, ~rep(.x, each = .y)), 
-               true_ocr = map(baseline_ocr, ~ .x * c(1, effect_size_injections))) %>% 
-        select(group_label, true_ocr) %>% 
-        unnest(group_label)
-    
-    # initialize the simulated plate data
-    plate.sim <- tibble(Well = well.id, 
-                        Group = "From which group?", 
-                        true_ocr = list(rep(background.ocr, times = num.injections + 1)), 
-                        bias_well = bias_well)
-    
-    plate.sim$Group[corner_wells] <- "Background"
-    plate.sim$Group[non_corner_wells] <- well_property$group_label
-    plate.sim$true_ocr[non_corner_wells] <- well_property$true_ocr
-    
-    
-    # simulate the true o2 and corresponding fluoresence data
-    plate.errorfree <- plate.sim %>% 
-        mutate(simulated_data = map(true_ocr, sim_o2_well)) %>% 
-        select(Well, Group, simulated_data, bias_well) %>% 
+    plate.sim.long <- plate.sim %>% 
+        select(-data) %>% 
         unnest(simulated_data)
+    
+    ## Sometimes after an injection, all the fluoresence values afterwards will be 
+    ## larger or smaller than what should be 
+    deviation_fluoresence_injection.df <- add_fluoresence_deviation_injection(plate_layout)
+    
+    deviation_fluoresence_injection.df.long <- deviation_fluoresence_injection.df %>% 
+        unnest(deviation_fluoresence_injection)
+    
+    plate.sim.complete.long <- plate.sim.long %>% 
+        left_join(deviation_fluoresence_injection.df.long, by = c("Well", "Group", "Measurement", "Tick"))
     
     # Every time the sensor registered a value, there is a measurement error
     # We assume that every registration is independent
-    num.registration <- nrow(plate.errorfree)
-    measurement_error <- rnorm(num.registration, mean = 0, sd = within_well_variation)
+    plate.sim.final <- plate.sim.complete.long %>% 
+        mutate(`O2 Corrected Em.` = pmap_dbl(list(true_emission_o2, deviation_fluoresence_well, deviation_fluoresence_injection), 
+                                            ~ rnorm(1, 
+                                                    mean = ..1 + ..2 + ..3, 
+                                                    sd = 10)))
     
-    plate.errorfree$measurement_error <- measurement_error
-    
-    # add the between well and within well variation to the true fluoresence
-    plate.sim <- plate.errorfree %>% 
-        mutate(`O2 Corrected Em.` = true_fluoresence + bias_well + measurement_error)
-    
-    return(plate.sim)
+    return(plate.sim.final)
 }
