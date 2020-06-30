2a7acd8aa3c1d0ddef72dd57c921545153206c98
Xiang Zhang <sc.xiang.zhang@gmail.com>
2020-06-30 22:38:18 +0200

    further simplify the code by using function add_mixing_wait_period and navigate_probe_position

--- R/sim_o2_well.R
+++ R/sim_o2_well.R
@@ -4,18 +4,14 @@
 #'
 #' @param ocr a vector of true biological oxygen consumption rates, pmol O2 / min
 #' @param tick.num the number of ticks within each measurement period
-#' @param tick.interval the time interval between two ticks in a measurement period, seconds
-#' @param mixing_wait_period the time interval between two measurement periods, seconds
 #' @import deSolve
 #' @import dplyr
 #' @import tibble
 #' @import purrr
 #' @import tidyr
-
+#' @export
 sim_o2_well <- function(ocr, 
-                        tick.num = 15, 
-                        tick.interval = 14, 
-                        mixing_wait_period = 120) {
+                        tick.num = 15) {
     TauP <- 43   # probe response time constant
     TauC <- 246  # From the wall to the medium
     TauAC <- 746 # From the atmosphere to the medium
@@ -31,24 +27,7 @@ sim_o2_well <- function(ocr,
     # between the last point of a measurement phase 
     # and the first point of the next measurement phase 
     # there is a "mixing & wait period"
-    dat.sim.init <- dat.sim.init %>% 
-        mutate(time = tick.interval * Tick + mixing_wait_period * (Measurement - 1))
-                               
-    mixing_wait_start <- dat.sim.init %>% 
-        filter(Measurement < num.measurement) %>% 
-        group_by(Measurement) %>% 
-        summarize(time = max(time))
-    
-    mixing_wait_end <- dat.sim.init %>% 
-        filter(Measurement > 1) %>% 
-        group_by(Measurement) %>% 
-        summarize(time = min(time))
-    
-    mixing_wait_period <- tibble(mixing_start = mixing_wait_start$time, 
-                                 mixing_end = mixing_wait_end$time) %>% 
-        mutate(time = map2(mixing_start, mixing_end, ~ seq(from = .x, to = .y, by = 1))) %>% 
-        select(time) %>% 
-        unnest(time)
+    mixing_wait_period <- add_mixing_wait_period(dat.sim.init)
     
     # OCR(t)
     ## step 1 create a time-series
@@ -60,16 +39,7 @@ sim_o2_well <- function(ocr,
     OCR <- approxfun(true_ocr_df, rule = 2)
     
     # probe_position(t)
-    ## When the probe is at the low position, measuring is on
-    probe_position_df <- data.frame(
-        time = seq(from = 0, to = max(dat.sim.init$time), by = 1), 
-        measuring_position = 1L
-    )
-    ## When the probe is at the high position, measuring is off
-    ## During this period, we assume that the diffusion rate from atomosphere to 
-    ## chamber will be larger
-    probe_position_df$measuring_position[probe_position_df$time %in% mixing_wait_period$time] <- 0L
-    probe_position <- approxfun(probe_position_df, rule = 2)
+    probe_position <- navigate_probe_position(dat.sim.init, mixing_wait_period)
     
     # initial states
     state <- c(O2M = o2_0, 
@@ -97,13 +67,12 @@ sim_o2_well <- function(ocr,
                     k_ac = 1 / TauAC, 
                     k_w = 1 / TauW, 
                     k_aw = 0, 
-                    Vc <- 20,           # Chamber volume ul
+                    Vc = 20,            # Chamber volume ul
                     k_ac_mixing = 0.05) # k_ac when the probe position is up 
     
     # Solve the ODE
     out <- ode(y = state, times = times, func = gerencser, parms = parameters) 
     dat.sim <- as.data.frame(out)
-        
     
     # Convert to O2 eimission
     # parameters to link o2 concentration to fluoresence
