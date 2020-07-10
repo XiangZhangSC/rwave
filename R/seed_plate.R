f9f216442f58dd3e24893b1b8258e8d298d1bc66
XiangZhangSC <sc.xiang.zhang@gmail.com>
2020-07-10 11:27:12 +0200

    Add cell number generator to each well

--- R/seed_plate.R
+++ R/seed_plate.R
@@ -3,11 +3,12 @@
 #' seed_plate produces a data frame with two columns Well and group Tag.
 #' 
 #' @param experiment_setup a data frame with two columns Group and n
+#' @param cell_number_per_well the targeted cell counts per well
 #' @import tibble
 #'
 #' @export
 
-seed_plate <- function(experiment_setup) {
+seed_plate <- function(experiment_setup, cell_number_per_well) {
     # plate set up
     well.row <- LETTERS[1:8]
     
@@ -24,12 +25,17 @@ seed_plate <- function(experiment_setup) {
     
     # initialize the simulated plate data
     plate.sim <- tibble(Well = well.id, 
-                        Group = "From which group?")
+                        Group = "From which group?", 
+                        cell_n = 0)
     
     plate.sim$Group[corner_wells] <- "Background"
     
     # every well represents a sample from a specific experimental group
     plate.sim$Group[non_corner_wells] <- tag_well(experiment_setup)
+    
+    # there is no cells in corner wells
+    # for other non corner wells, the cell numbers fluctuate around the target number
+    plate.sim$cell_n[non_corner_wells] <- rpois(92, lambda = cell_number_per_well)
     
     return(plate.sim)
 }
