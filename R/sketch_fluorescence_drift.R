9bd41324d59fd93fde97c6a537cba18a5f390db9
XiangZhangSC <sc.xiang.zhang@gmail.com>
2020-08-17 16:50:58 +0200

    added plot function for fluorescence drift amount

--- R/sketch_fluorescence_drift.R
+++ R/sketch_fluorescence_drift.R
@@ -3,13 +3,25 @@
 #' A significant drift will be highlighted by a red dot, otherwise it is black
 #'
 #' @param df is a data frame produced by \code{qc_fluorescence_drift}
+#' @param drift.constant is a logic value indicates whether only plot drift constants
+#' @param num.ticks a numeric value produced by \code{summarize_assay}
 #'
 #' @export
-sketch_fluorescence_drift <- function(df) {
-    ggplot(df, aes(as.factor(Measurement), Estimate)) + 
-        geom_point() + 
-        geom_point(data = filter(my_drift_constants, p.value < 0.05), color = "red") + 
-        facet_wrap(~Well) + 
-        labs(x = "Measurement", y = "Drift constant") + 
-        theme_bw()
+sketch_fluorescence_drift <- function(df, drift.constant = TRUE, num.ticks) {
+    
+    if (drift.constant == TRUE) {
+        ggplot(df, aes(as.factor(Measurement), Estimate)) + 
+            geom_point() + 
+            geom_point(data = filter(df, p.value < 0.05), color = "red") + 
+            facet_wrap(~Well) + 
+            labs(x = "Measurement", y = "Drift constant") + 
+            theme_bw()
+    } else {
+        summarize_fluorescence_drift(df, num.ticks = num.ticks, stats.only = FALSE) %>% 
+            ggplot(aes(as.factor(Tick2), fluorescence_drift)) + 
+            geom_boxplot() + 
+            facet_wrap(~Measurement) + 
+            theme_bw() + 
+            labs(x = "Tick", y = "Fluorescence drift")
+    }
 }
